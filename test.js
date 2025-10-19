70c4395fd4a7df772f48dbd91f341258ea965975
涵曦 <im.hanxi@gmail.com>
2025-10-19 09:21:11 +0800

    update test

--- test.js
+++ test.js
@@ -1,24 +1,26 @@
 var fs = require("fs");
 var sproto = require("./sproto");
 
-var filename = "./sproto-js/protocol.spb";
+var filename = "./protocol.spb";
 var buffer = fs.readFileSync(filename);
-if (buffer == null){
-	console.log("read File err1");
+if (buffer == null) {
+  console.log("read File err1");
 }
 console.log(sproto);
 
 var sp = sproto.createNew(buffer);
 console.log(sp);
 
+let client = sp.host("base.package");
+let client_request = client.attach(sp);    //获取一个request请求的回调函数
+let data = {
+  token: "test",
+  ctx: {
+    proto_checksum: "xxxxx"
+  }
+};
+let req = client_request("login.login", data);
+// req 用于发送给服务器
 
-var basicType = {
-	numberList: []
-}
-var buffer = sp.encode("base.BasicType", basicType)
-console.log(buffer)
-
-// var new_buffer = [ 2,0,5,0,0,0,0,0,0,0 ]
-
-var result = sp.decode("base.BasicType", buffer)
-console.log(result)
+let ret = client.dispatch(req);    //这个对应于 host:dispatch(req)
+console.log("ret", ret);       //打印数据
